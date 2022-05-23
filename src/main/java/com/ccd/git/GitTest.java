8b6acf24da404fcb9903c32adabf76b7481e6db3
CCD <CCD@ikun.com>
2022-05-23 22:23:04 +0800

    version 1

---
+++ src/main/java/com/ccd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ccd.git;
+
+/**
+ * @author :ccd
+ * @date : 2022/5/23 22:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+    }
+}
